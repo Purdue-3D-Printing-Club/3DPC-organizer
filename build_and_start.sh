57dff403a1a5c2596d7e48afbd6d704c235f8fb4
ConTejas624 <tjgeorge99@gmail.com>
2023-05-17 12:41:14 -0400

    add default for container debugging script Do this so I don't have to type versions everytime I run it

--- build_and_start.sh
+++ build_and_start.sh
@@ -8,16 +8,16 @@ fi
 
 # Check if both solution name and version number are provided as command line arguments
 if [ $# -ne 2 ]; then
-    echo "Please provide the solution name and version number as command line arguments."
-    exit 1
+    # Set default container name
+    container_name="organizer-tmp"
+else
+    # Set the name of the solution and version number from the command line arguments
+    solution_name="$1"
+    version_number="$2"
+
+    # Set the name of the container with the version number appended
+    container_name="${solution_name}v${version_number}"
 fi
-
-# Set the name of the solution and version number from the command line arguments
-solution_name="$1"
-version_number="$2"
-
-# Set the name of the container with the version number appended
-container_name="${solution_name}v${version_number}"
 
 # Build the Docker container
 docker build -t "${container_name}" .
