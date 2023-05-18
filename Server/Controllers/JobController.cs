c92c7dae53f69086f3d2c6d82edec608fd3c8208
ConTejas624 <tjgeorge99@gmail.com>
2023-05-17 22:32:04 -0400

    Get for JobController

--- Server/Controllers/JobController.cs
+++ Server/Controllers/JobController.cs
@@ -1,5 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
-using Organizer.Shared;
+using Organizer.Shared.Models;
 
 namespace Organizer.Server.Controllers;
 
@@ -7,9 +7,52 @@ namespace Organizer.Server.Controllers;
 [Route("api/job")]
 public class JobController : ControllerBase
 {
+    private static Job[] temp_jobs = { // temporary until I get a database setup
+        new Job {
+            id = "job1",
+            userSubmittingId = "usr1",
+            supervisorId = "sup1",
+            status = JobState.Pending,
+            files = new string[] { "filepath1" },
+            notes = "notes",
+            estimatedFilament = 1.2f
+        },
+        new Job {
+            id = "job2",
+            userSubmittingId = "usr2",
+            supervisorId = "sup2",
+            status = JobState.InProgress,
+            files = new string[] { "filepath1", "filepath2" },
+            notes = "notes",
+            estimatedFilament = 123.45f
+        },
+    };
     private readonly ILogger<JobController> _logger;
     public JobController(ILogger<JobController> logger)
     {
         _logger = logger;
+    }
+
+    [HttpGet]
+    public Job[] Get()
+    {
+        return temp_jobs;
+    }
+
+    [HttpGet("{id}")]
+    public Job? Get(string? id)
+    {
+        if (id == null)
+        {
+            return null;
+        }
+        for (int i = 0; i < temp_jobs.Length; i++)
+        {
+            if (temp_jobs[i].id == id)
+            {
+                return temp_jobs[i];
+            }
+        }
+        return null;
     }
 }
